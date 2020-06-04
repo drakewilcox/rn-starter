cbe81863d6ea77bd79b164b4adc59cbc9461338d
Drake Wilcox <drakewilcox@gmail.com>
2020-06-04 14:58:39 -0700

    add functionality to text screen and wire up BoxScreen.

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
+  const [password, setPassword] = useState('');
+
   return (
     <View>
-      <Text>Text Screen</Text>
+      <Text>Enter Password: </Text>
+      <TextInput 
+        style={styles.input} 
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={password}
+        onChangeText={(newValue) => setPassword(newValue)}
+      />
+      {password.length < 4 ? <Text>Password must be at least 4 characters</Text> : null }
     </View>
   )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    margin: 15, 
+    borderColor: 'black',
+    borderWidth: 1
+  }
+});
 
 export default TextScreen; 
