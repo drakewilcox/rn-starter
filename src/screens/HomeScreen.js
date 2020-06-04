f2e0e9bfd8ecf8e6fed25b67895fdbd04e86cdfe
Drake Wilcox <drakewilcox@gmail.com>
2020-06-04 07:44:05 -0700

    add components for counterscreen.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,6 +3,7 @@ import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
   
+
   return (
     <View>
       <Text style={styles.text}>HomeScreen</Text>
@@ -17,6 +18,10 @@ const HomeScreen = ({ navigation }) => {
       <Button 
         title="Go to Image Demo"
         onPress={() => navigation.navigate('Image')} 
+      />
+       <Button 
+        title="Go to Counter Demo"
+        onPress={() => navigation.navigate('Counter')} 
       />
     </View>
   )
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import ImageDetail from '../components/ImageDetail'
+
 
 const ImageScreen = () => {
-  return <Text>Image Screen</Text>;
+  return (
+    <View>
+      <ImageDetail 
+        title="Forest" 
+        imageSource={require('../../assets/beach.jpg')}
+        score={9}
+      />
+      <ImageDetail 
+        title="Beach" 
+        imageSource={require('../../assets/mountain.jpg')}
+        score={7}
+      />
+      <ImageDetail
+        title="Mountain" 
+        imageSource={require('../../assets/forest.jpg')}
+        score={10}
+      />
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({});
